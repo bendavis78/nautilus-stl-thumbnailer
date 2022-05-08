745b052b1bac2d2820a33a18d58e4519f0615138
Ben Davis <bendavis78@gmail.com>
2022-05-08 18:18:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# STL thumbnailer for nautilus
+
+Requires: openscad
+
+## Setup
+sudo cp stl-thumbnailer.sh /usr/local/bin/
+sudo cp stl.thumbnailer /usr/share/thumbnailers/
---
+++ stl-thumbnailer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+infile="$1"
+outfile="$2"
+size="$3"
+
+Xvfb :99 &
+PID=$!
+export DISPLAY=":99"
+
+echo "/usr/bin/openscad /dev/null -D \"color([1, .5, 0]) import(\\\"$infile\\\");\" -o \"$outfile\" --imgsize=\"${size},${size}\""
+/usr/bin/openscad /dev/null -D "color([1, .5, 0]) import(\"$infile\");" -o "$outfile" --imgsize="${size},${size}" 2>&1
+
+kill $PID
